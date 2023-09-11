861531c0b1a9217747517b51a29236bc596a111d
Elianderson Costa <eliandersonscosta@gmail.com>
2023-09-10 21:20:29 -0300

    Description to Terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,10 +11,10 @@ variable "public-key-name" {
 variable "public-key" {
   type = string
   default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDc3HnNYlQY5PUi7iR9wZ8Us1qNavyiIqm+2FchtMA1b2q1O6QyseQej510NPq7uYWki3MCtKAoLbBUz/XPGS+JMPg4C5OrYmeofw78SSBPo6W9P9BXmUXG3zE+dWGJDQyrsZIww9Q178bSmUyeK8bqmIauj8sE1k0WjrfQC8a8ih6BC5BPvrpylly1GEvgD+fONLqr8uc+040LixRvIGdQIyZcFvR0Wf0dRE4apY216vFyu4lohPfkkJKUasCOSZ4OUxWesuaW+7iIXxnjr5s8YGBOX48cm9A2iWXJrmtIDslKIAk7NUkZ1FukFyrqMUNp+yvTrcr3gzAIOOdLvZA5"
+  description = "This key is only for learning purposes"
 }
 
 variable "image-id" {
   type = string
   default = "ami-0261755bbcb8c4a84"
 }
-
