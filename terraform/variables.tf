cb416230323329e5878d7c85de40de1c0d634b6c
3lianderson <elianderson.costa@gmail.com>
2023-09-19 22:46:33 -0300

    GitIgnore and variable changes

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,20 +1,20 @@
 variable "aws-region" {
-  type    = string
-  default = "us-east-1"
+  type        = string
+  description = "Region used to bootstrap the cluster"
 }
 
 variable "public-key-name" {
-  type = string
-  default = "aws-key"
+  type        = string
+  description = "Public Key Name used to bootstrap the cluster"
 }
 
 variable "public-key" {
-  type = string
-  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDc3HnNYlQY5PUi7iR9wZ8Us1qNavyiIqm+2FchtMA1b2q1O6QyseQej510NPq7uYWki3MCtKAoLbBUz/XPGS+JMPg4C5OrYmeofw78SSBPo6W9P9BXmUXG3zE+dWGJDQyrsZIww9Q178bSmUyeK8bqmIauj8sE1k0WjrfQC8a8ih6BC5BPvrpylly1GEvgD+fONLqr8uc+040LixRvIGdQIyZcFvR0Wf0dRE4apY216vFyu4lohPfkkJKUasCOSZ4OUxWesuaW+7iIXxnjr5s8YGBOX48cm9A2iWXJrmtIDslKIAk7NUkZ1FukFyrqMUNp+yvTrcr3gzAIOOdLvZA5"
-  description = "This key is only for learning purposes"
+  type        = string
+  description = "Public Key used to bootstrap the cluster"
 }
 
 variable "image-id" {
-  type = string
-  default = "ami-0261755bbcb8c4a84"
+  type        = string
+  description = "Image Id used to bootstrap the cluster"
+
 }
